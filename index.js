55a2363a0a22c8cfc530018688e428195c92a558
Sigurd Fosseng <sigurd.fosseng@sparebank1.no>
2016-01-28 09:48:07 +0100

    Add a deperecation check to the framework

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/**
+ * @deprecated since version 1.0.0
+ */
 exports.printMessage = function () {
     console.log("This JavaScript framework is deprecated.");
+};
+
+/**
+ * Check the deprecation status of a npm module
+ *
+ * @param {string} npmModule - The name of the npm module
+ * @deprecated since version 1.0.0
+ */
+exports.isDeprecated = function(npmModule) {
+    return true;
 };
--- test.js
+++ test.js
@@ -1,2 +1,5 @@
+var assert = require('assert');
 var Deprecated = require('./index');
 Deprecated.printMessage();
+
+assert(Deprecated.isDeprecated('deprecated.js'), 'should be deprecated');
